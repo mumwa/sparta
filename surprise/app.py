bbed8076f6b0bca0e01829d8c43287f79993cd1d
박효 <mumwa@naver.com>
2020-06-13 18:17:01 +0900

    프로필 등록 구현, 프로필 읽어오기 연결

--- surprise/app.py
+++ surprise/app.py
@@ -1,53 +1,48 @@
-from pymongo import MongoClient, ASCENDING
-
 from flask import Flask, render_template, jsonify, request
 app = Flask(__name__)
 
-client = MongoClient('localhost', 27017)
-db = client.dbsparta
+from pymongo import MongoClient           # pymongo를 임포트 하기(패키지 인스톨 먼저 해야겠죠?)
+client = MongoClient('localhost', 27017)  # mongoDB는 27017 포트로 돌아갑니다.
+db = client.dbsurprise                    # 'dbsparta'라는 이름의 db를 만듭니다.
 
-
-# HTML을 주는 부분
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('search.html')
 
-# API 역할을 하는 부분
-@app.route('/api/list', methods=['GET'])
-def stars_list():
-    print('asdfasdf')
-    star_cursor=db.mystar.find({},{'_id':False}).sort('like', -1)
-    star_list=list(star_cursor)
-    print(star_list)
-    return jsonify({'result':'success', 'msg':'list 변경되었습니다!', 'star_list':star_list})
-    # 1. mystar 목록 전체를 검색합니다. ID는 제외하고 like 가 많은 순으로 정렬합니다.
-    # 참고) find({},{'_id':False}), sort()를 활용하면 굿!
-    # 2. 성공하면 success 메시지와 함께 stars_list 목록을 클라이언트에 전달합니다.
-
-
-@app.route('/api/like', methods=['POST'])
-def star_like():
-    # 1. 클라이언트가 전달한 name_give를 name_receive 변수에 넣습니다.
-    name_recieve = request.form['name_give']
-    # 2. mystar 목록에서 find_one으로 name이 name_receive와 일치하는 star를 찾습니다.
-    found_star=db.mystar.find_one({'name':name_recieve})
-    # 3. star의 like 에 1을 더해준 new_like 변수를 만듭니다.
-    new_like=found_star['like']+1
-    # 4. mystar 목록에서 name이 name_receive인 문서의 like 를 new_like로 변경합니다.
-    db.mystar.update({'name':name_recieve}, {'$set':{'like':new_like}})
-    # 참고: '$set' 활용하기!
-    # 5. 성공하면 success 메시지를 반환합니다.
-    return jsonify({'result': 'success', 'msg':'like 연결되었습니다!'})
-
-
-@app.route('/api/delete', methods=['POST'])
-def star_delete():
-    # 1. 클라이언트가 전달한 name_give를 name_receive 변수에 넣습니다.
-    name_receive = request.form['name_give']
-    db.mystar.delete_one({'name':name_receive})
-    # 2. mystar 목록에서 delete_one으로 name이 name_receive와 일치하는 star를 제거합니다.
-    # 3. 성공하면 success 메시지를 반환합니다.
+@app.route('/profilelist', methods=['GET'])
+def read_profile():
+    profiles = list(db.profiles.find({},{'_id':0}))
     return jsonify({'result': 'success'})
 
-if __name__ == '__main__':
-    app.run('0.0.0.0', port=5000, debug=True)
+@app.route('/registration')
+def registration_page():
+    return render_template('registration.html')
+
+@app.route('/registration', methods=['POST'])
+def new_profile():
+    # title_receive로 클라이언트가 준 title 가져오기
+    print(request.form)
+    name_receive = request.form['name_give']
+    phonenumber_receive = request.form['phonenumber_give']
+    birthday_receive = request.form['birthday_give']
+    bank_receive = request.form['bank_give']
+    account_receive = request.form['account_give']
+    address_receive = request.form['address_give']
+    password_receive = request.form['password_give']
+
+    # DB에 삽입할 review 만들기
+    profile = {
+        'name': name_receive,
+        'phonenumber': phonenumber_receive,
+        'birthday': birthday_receive,
+        'bank': bank_receive,
+        'account': account_receive,
+        'address': address_receive,
+        'password': password_receive,
+    }
+    # reviews에 review 저장하기
+    db.profiles.insert_one(profile)
+    # 성공 여부 & 성공 메시지 반환
+    return jsonify({'result': 'success', 'msg': '프로필이 등록되었습니다.'})
+if __name__ == '__main__':  
+    app.run('0.0.0.0',port=5000,debug=True)
